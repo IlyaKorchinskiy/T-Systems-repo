f5f2d578bf343d2bfd33701835679c29bcb7149a
KorchinskiyIlya <korchinskiy.ilya@gmail.com>
2019-12-08 20:45:24 +0300

    add order service

---
+++ InternetStore/orderservice/src/main/java/ru/korchinskiy/app/Application.java
@@ -0,0 +1,8 @@
+package ru.korchinskiy.app;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication(scanBasePackages = "ru.korchinskiy")
+public class Application extends SpringBootServletInitializer {
+}
