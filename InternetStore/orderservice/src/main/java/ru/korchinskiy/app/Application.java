8d013d5addadeb05cc8daa60d392ef73e37298f4
KorchinskiyIlya <korchinskiy.ilya@gmail.com>
2019-12-14 18:45:49 +0300

    order-service deploy changes

--- InternetStore/orderservice/src/main/java/ru/korchinskiy/app/Application.java
+++ InternetStore/orderservice/src/main/java/ru/korchinskiy/app/Application.java
@@ -1,8 +1,12 @@
 package ru.korchinskiy.app;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(scanBasePackages = "ru.korchinskiy")
-public class Application extends SpringBootServletInitializer {
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
